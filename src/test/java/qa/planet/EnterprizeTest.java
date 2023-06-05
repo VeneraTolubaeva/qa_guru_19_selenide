69614bf35186e5f8721204920019aa7ae8453400
VeneraTolubaeva <venera.to@zohomail.com>
2023-06-05 21:57:03 +0300

    Fixed EnterprizeTest

--- src/test/java/qa/planet/EnterprizeTest.java
+++ src/test/java/qa/planet/EnterprizeTest.java
@@ -9,10 +9,10 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class EnterprizeTest extends Config {
     @Test
-    public void EnterprizeTest() {
+    public void enterprizeTest() {
         //На главной странице GitHub выберите меню Solutions->Enterprize с помощью команды hover для Solutions
         open("https://github.com");
-        $(".Header-old").$(byText("Solutions")).click();
+        $(".Header-old").$(byText("Solutions")).hover();
         $(".border-bottom").$(byText("Enterprise")).click();
 
         // Убедитесь что загрузилась нужная страница (например, что заголовок - "Build like the best.")
